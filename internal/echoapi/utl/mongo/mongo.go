c6b9f6d2039ed253ab94b3e1e8e7318f4585ab6d
Reopard226 <robcoder@hotmail.com>
2020-01-23 15:29:26 +0100

    added mongo connection check

--- internal/echoapi/utl/mongo/mongo.go
+++ internal/echoapi/utl/mongo/mongo.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
 )
 
 // APIKEY_COLLECTION_NAME Collection name
@@ -27,6 +29,13 @@ func NewMongoDatabase(connString string, database string) (*mongo.Database, erro
 	}
 	// Collection types can be used to access the database
 	dbInit = client.Database(database)
+
+	// check the mongodb connection
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
 
 	return dbInit, nil
 
